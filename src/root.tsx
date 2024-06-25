5a9df8c2c353dd9de6d8c8a82f5f5906bf0e75a1
Evan Douglass <evanldouglass@gmail.com>
2024-06-24 21:23:04 -0700

    Add Cloudflare Pages configuration files and update Vite config. Minor blog updates.

--- src/root.tsx
+++ src/root.tsx
@@ -5,9 +5,48 @@ import {
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city"
 import { RouterHead } from "./components/router-head/router-head"
-import setInitialThemeAsString from "./utils/setInitialTheme?raw"
 
 import "./global.css"
+
+const themeStartupCode = `
+  const LOCAL_STORAGE_KEY = "theme"
+  const SYSTEM = "system"
+  const DARK = "dark"
+  const LIGHT = "light"
+
+  const watcher = window.matchMedia("(prefers-color-scheme: dark)")
+
+  function setSystemTheme() {
+    watcher.matches
+      ? document.documentElement.classList.add(DARK)
+      : document.documentElement.classList.remove(DARK)
+  }
+
+  // Listen for system theme changes
+  watcher.onchange = () => {
+    console.log("System theme changed")
+    const theme = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (theme === SYSTEM) {
+      setSystemTheme()
+    }
+  }
+
+  // Set initial theme
+  const theme = localStorage.getItem(LOCAL_STORAGE_KEY)
+  switch (theme) {
+    case DARK:
+      document.documentElement.classList.add(DARK)
+      break
+    case LIGHT:
+      document.documentElement.classList.remove(DARK)
+      break
+    case SYSTEM:
+    default:
+      setSystemTheme()
+      localStorage.setItem(LOCAL_STORAGE_KEY, SYSTEM)
+      break
+  }
+`
 
 export default component$(() => {
   /**
@@ -24,8 +63,8 @@ export default component$(() => {
         <RouterHead />
         {/* Initial theme set here to avoid flash of unstyled content */}
         <script
-          type="module"
-          dangerouslySetInnerHTML={setInitialThemeAsString}
+          type="text/javascript"
+          dangerouslySetInnerHTML={themeStartupCode}
         ></script>
       </head>
       <body lang="en" class="bg-zinc-50 dark:bg-zinc-900">
