cebf800cff1438a707b4123013396ca69e97fcca
Evan Douglass <evanldouglass@gmail.com>
2024-06-29 21:34:14 -0700

    Update footer with improved copyright statement

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import { component$ } from "@builder.io/qwik"
 export const Footer = component$(() => {
   return (
     <footer class="my-12 text-xs text-text-dark sm:my-24 sm:px-8 dark:text-text-light">
-      <p>&copy; 2024 Evan Douglass | Seattle, WA</p>
+      <p>&copy; 2024 Evan Douglass. All rights reserved.</p>
       <p>
         <a href="mailto:evandouglass@eldss.dev">evandouglass@eldss.dev</a>
       </p>
